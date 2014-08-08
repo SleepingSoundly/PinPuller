963e797ab05ec381b0faeb3d97ef537dfb2b74cc
SleepingSoundly <hoag3@illinois.edu>
2014-08-08 13:53:26 -0500

    Update classPin.py

--- classPin.py
+++ classPin.py
@@ -2,5 +2,5 @@
 #				PinPuller 
 #					class: pin, board, user
 #
-#
+#  <div>Whoops! We couldn't find that page.</div> is the error we search for.....
 ###############################################################
